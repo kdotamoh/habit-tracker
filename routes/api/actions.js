fa68a1348922361696fe8c3d2e07f15d1e9330aa
kwadwo-amoh <21177558+kdotamoh@users.noreply.github.com>
2019-06-26 16:18:32 +0000

    Get/delete action

--- routes/api/actions.js
+++ routes/api/actions.js
@@ -30,8 +30,31 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
-  res.send("Not implemented");
+router.get("/:id", async (req, res) => {
+  let { id } = req.params;
+
+  try {
+    const action = await db.models.Action.findByPk(id);
+    res.send(action);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    let action = await db.models.Action.findByPk(id);
+    if (action) {
+      await action.destroy();
+      res.status(202).send("Success");
+    } else {
+      res.status(404).send("Not found");
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
 router.delete("/:id", (req, res) => {
